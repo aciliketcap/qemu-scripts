5af16e425a3fc269cc8315d396e03fb66d0a196f
Sinan Akpolat <aciliketcap@gmail.com>
2015-08-12 23:45:51 +0300

    Run echo command to /proc correctly
    Script was redirecting sudo output. Now it tells sudo to run command with redirection.

--- startinternet.sh
+++ startinternet.sh
@@ -3,5 +3,5 @@
 sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 sudo ip ad ad 192.168.1.1/24 dev tap0
 sudo ip link set tap0 up
-sudo "echo 1 > /proc/sys/net/ipv4/ip_forward"
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 sudo /etc/init.d/dnsmasq start
